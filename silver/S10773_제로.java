6fa836399c6b0918d2069980751568b5b95872e2
shin-yeseong <1020blue@dongguk.edu>
2025-07-18 17:36:30 +0900

    백준 실버 10773번 제로 문제풀이 추가

---
+++ silver/S10773_제로.java
@@ -0,0 +1,32 @@
+package silver;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.Stack;
+import java.io.IOException;
+
+public class S10773_제로 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(br.readLine());
+            if (value == 0) {
+                stack.pop();
+            } else {
+                stack.push(value);
+
+            }
+        }
+        int sum = 0;
+        for (int num : stack) {
+            sum += num;
+        }
+        System.out.print(sum);
+    }
+}
