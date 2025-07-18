ec2465824c4e5c7b0a69199478f46202db795669
shin-yeseong <1020blue@dongguk.edu>
2025-07-18 22:53:24 +0900

    백준 1874번 스택수열 문제 업로드

---
+++ silver/S1874_스택수열.java
@@ -0,0 +1,46 @@
+package silver;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.Stack;
+import java.io.IOException;
+
+public class S1874_스택수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+
+        }
+        int current = 1;
+        boolean isPossible = true;
+
+        for (int j = 0; j < N; j++) {
+            int target = arr[j];
+
+            while (current <= target) {
+                stack.push(current++);
+                sb.append("+ \n");
+            }
+            if (stack.peek() == target) {
+                stack.pop();
+                sb.append("- \n");
+            } else {
+                isPossible = false;
+                break;
+            }
+
+        }
+        if (isPossible) {
+            System.out.print(sb);
+        } else {
+            System.out.print("NO");
+        }
+    }
+}
